5cb7fc3d97e752c6496ac5d98fdd7adbe78f5479
Rafael Fuentes Cantero <rafafuentesdev@gmail.com>
2023-10-08 10:46:44 +0200

    Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -45,9 +45,8 @@
                                 <router-link v-for="item in navigation" :key="item.name" :to="item.href" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">{{ item.name }}</router-link>
                             </div>
                             <div class="py-6">
-                                <a href="#"
-                                    class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log
-                                    in</a>
+                            <router-link to="/about-us"
+                                class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">About Us</router-link>
                             </div>
                         </div>
                     </div>
