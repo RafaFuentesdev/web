6681a3bff903864f35ea973fc8f1bb59677dcd5f
Rafael Fuentes Cantero <rafafuentesdev@gmail.com>
2023-10-08 15:17:30 +0200

    inquire

--- src/pages/Inquire.vue
+++ src/pages/Inquire.vue
@@ -1,8 +1,82 @@
 <script setup>
 import Hero from '../components/Hero.vue'
-import Map from '../components/Map.vue'
+
+// Data for Málaga
+const inquiries = [
+    { Año: 1990, Conatos: 215, Incendios: 172, Total: 387 },
+    { Año: 1991, Conatos: 167, Incendios: 147, Total: 314 },
+    { Año: 1992, Conatos: 164, Incendios: 140, Total: 304 },
+    { Año: 1993, Conatos: 149, Incendios: 123, Total: 272 },
+    { Año: 1994, Conatos: 98, Incendios: 87, Total: 185 },
+    { Año: 1995, Conatos: 69, Incendios: 50, Total: 119 },
+    { Año: 1996, Conatos: 21, Incendios: 10, Total: 31 },
+    { Año: 1997, Conatos: 37, Incendios: 16, Total: 53 },
+    { Año: 1998, Conatos: 58, Incendios: 37, Total: 95 },
+    { Año: 1999, Conatos: 53, Incendios: 23, Total: 76 },
+    { Año: 2000, Conatos: 61, Incendios: 26, Total: 87 },
+    { Año: 2001, Conatos: 73, Incendios: 40, Total: 113 },
+    { Año: 2002, Conatos: 77, Incendios: 47, Total: 124 },
+    { Año: 2003, Conatos: 71, Incendios: 24, Total: 95 },
+    { Año: 2004, Conatos: 85, Incendios: 34, Total: 119 },
+    { Año: 2005, Conatos: 78, Incendios: 42, Total: 120 },
+    { Año: 2006, Conatos: 53, Incendios: 25, Total: 78 },
+    { Año: 2007, Conatos: 57, Incendios: 23, Total: 80 },
+    { Año: 2008, Conatos: 63, Incendios: 12, Total: 75 },
+    { Año: 2009, Conatos: 68, Incendios: 28, Total: 96 },
+    { Año: 2010, Conatos: 34, Incendios: 22, Total: 56 },
+    { Año: 2011, Conatos: 51, Incendios: 20, Total: 71 },
+    { Año: 2012, Conatos: 87, Incendios: 23, Total: 110 },
+    { Año: 2013, Conatos: 79, Incendios: 31, Total: 110 },
+    { Año: 2014, Conatos: 66, Incendios: 20, Total: 86 },
+    { Año: 2015, Conatos: 57, Incendios: 30, Total: 87 },
+    { Año: 2016, Conatos: 73, Incendios: 17, Total: 90 },
+    { Año: 2017, Conatos: 79, Incendios: 32, Total: 111 },
+    { Año: 2018, Conatos: 51, Incendios: 13, Total: 64 },
+    { Año: 2019, Conatos: 85, Incendios: 25, Total: 110 },
+    { Año: 2020, Conatos: 57, Incendios: 21, Total: 78 },
+    { Año: 2021, Conatos: 34, Incendios: 18, Total: 52 }
+];
+
+
 </script>
 
 <template>
     <Hero />
-</template>
+    <div class="px-4 sm:px-6 lg:px-8 bg-white">
+        <div class="sm:flex sm:items-center">
+            <div class="sm:flex-auto">
+                <h1 class="font-semibold leading-6 text-gray-900 text-xl">Inquiries</h1>
+                <p class="mt-2 text-gray-700 text-m">A list of inquiries by year and province including the number of
+                    conatos, incendios, and the total.</p>
+            </div>
+            <!-- You can add more controls here if needed -->
+        </div>
+        <div class="mt-8 flow-root">
+            <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-2 lg:-mx-4">
+                <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                    <table class="min-w-full divide-y divide-gray-300">
+                        <thead>
+                            <tr>
+                                <th scope="col"
+                                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">Year</th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Conatos
+                                </th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Incendios
+                                </th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Total</th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white">
+                            <tr v-for="inquiry in inquiries" :key="inquiry.Año" class="even:bg-gray-50">
+                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">{{
+                                    inquiry.Año }}</td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ inquiry.Conatos }}</td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ inquiry.Incendios }}</td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ inquiry.Total }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+    </div>
+</div></template>
--- src/pages/Prevention.vue
+++ src/pages/Prevention.vue
@@ -3,27 +3,29 @@ import Hero from '../components/Hero.vue'
 </script>
 
 <template>
-    <div class="bg-white py-8">
+    <div class="bg-white">
         <!-- Hero Section -->
-        <Hero title="Fire Prevention" />
+        <Hero />
 
         <!-- Content Section -->
         <div class="container mx-auto px-4 text-black">
-            <h2 class="text-2xl font-bold mb-6 text-center">Fire Prevention in Andalucía</h2>
-            <p class="text-lg mb-6">
-                Wildfires are a significant concern, especially during the dry months in Andalucía. Here are some tips to
-                help prevent fires and protect our beautiful region.
+            <h2 class="font-bold mb-6 text-center text-4xl">Wilting Point Forecast - 08/10/2023</h2>
+            <p class="text-xl mb-6">
+                Wildfires are a pressing issue in Andalucía, particularly during the arid months. A key metric aiding in
+                wildfire prediction is the wilting point of the soil, which relates to soil moisture and vegetation health.
             </p>
 
             <!-- Image of the Wilting Point -->
-            <div class="flex justify-center mb-6">
-                <img src="/wiltingpoint.png" alt="Wilting Point in Andalucía" class="rounded-lg shadow-md">
+            <div class="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                <img src="/wiltingpoint.png" alt="Wilting Point Map" width="2432" height="1442"
+                    class="rounded-md shadow-2xl ring-1 ring-gray-900/10" />
             </div>
 
-            <p class="text-lg">
-                The image above shows the wilting point in Andalucía, a crucial metric in understanding soil moisture and
-                vegetation health. Monitoring such metrics can aid in predicting potential fire outbreaks and taking
+            <p class="p-8 text-lg">
+                The map illustrates the number of days until each plot in Andalucía reaches its wilting point. When this
+                value is near 0, it indicates that the soil moisture is critically low, making the region more susceptible
+                to wildfires. Regularly monitoring these values empowers us to anticipate potential fire risks and implement
                 preventive measures.
             </p>
-        </div>
-    </div></template>
+    </div>
+</div></template>
